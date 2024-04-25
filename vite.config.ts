0f69883efacb905a9448c772c5a58240bdd85383
olesiklesha <olesik736@gmail.com>
2024-04-25 11:01:33 +0300

    core: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
     plugins: [react()],
+    base: '/cleverFIT',
     server: {
         host: true,
         port: 3000,
